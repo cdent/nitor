01e2df4086467d1377894528d14106b7d32943e5
Chris Dent <chris.dent@gmail.com>
2012-03-11 18:50:14 +0000

    Use do_html decorator to run start_response

--- tiddlywebplugins/nitor/__init__.py
+++ tiddlywebplugins/nitor/__init__.py
@@ -5,7 +5,7 @@ Winging it.
 """
 
 from tiddlyweb.util import merge_config
-from tiddlywebplugins.utils import replace_handler
+from tiddlywebplugins.utils import replace_handler, do_html
 from tiddlywebplugins.nitor.config import config as plugin_config
 from tiddlywebplugins.nitor.template import send_template
 
@@ -13,6 +13,7 @@ from tiddlywebplugins.nitor.template import send_template
 __version__ = '0.0.1'
 
 
+@do_html()
 def home(environ, start_response):
     return send_template(environ, 'home.html', { 'title': 'Welcome'})
 
